bb065e5044d203fab8d3a35ff59592ea579d384a
Andrei Gatej <andreigtj01@gmail.com>
2024-01-17 22:06:09 +0200

    feat: add base neovim config

---
+++ nvim/lua/plugins/dap-ui.lua
@@ -0,0 +1,22 @@
+if true then
+  return {}
+end
+
+return {
+  "rcarriga/nvim-dap-ui",
+  config = function(_, opts)
+    require("dap.ext.vscode").load_launchjs()
+    local dap = require("dap")
+    local dapui = require("dapui")
+    dapui.setup(opts)
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open({})
+    end
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+      dapui.close({})
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      dapui.close({})
+    end
+  end,
+}
