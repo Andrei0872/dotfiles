7432a4804f7b9b99b1a1224aabbda9bf541510b0
Andrei Gatej <andreigtj01@gmail.com>
2024-01-19 21:23:16 +0200

    feat: add linting & formatting extras

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -16,6 +16,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
