77d88fb9923649edc7658b1bdc4d76e8a8a96e6f
Andrei Gatej <andreigtj01@gmail.com>
2024-01-27 21:17:15 +0200

    feat(autocomplete): select item on tab as well

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -32,6 +32,7 @@ return {
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
       }),
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
