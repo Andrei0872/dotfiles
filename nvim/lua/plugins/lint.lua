efc0d524ddff23de932781b0f523952c357eb22f
Andrei Gatej <andreigtj01@gmail.com>
2024-01-27 21:49:28 +0200

    temp(lint): remove `luacheck`

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -12,7 +12,6 @@ return {
         typescript = { "eslint_d" },
         javascriptreact = { "eslint_d" },
         typescriptreact = { "eslint_d" },
-        lua = { "luacheck" }
       }
 
       local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
