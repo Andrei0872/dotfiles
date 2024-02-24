915b8073aafd137a243be32faade4b53d6e9a5bc
Andrei Gatej <andreigtj01@gmail.com>
2024-02-24 21:18:55 +0200

    feat(plugins): add `lualine`

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,28 +1,44 @@
 return {
-	{
-		"echasnovski/mini.pairs",
-		version = false,
-		event = { "InsertEnter" },
-		config = function()
-			require("mini.pairs").setup()
-		end,
-	},
+  {
+    "echasnovski/mini.pairs",
+    version = false,
+    event = { "InsertEnter" },
+    config = function()
+      require("mini.pairs").setup()
+    end,
+  },
 
-	{
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		lazy = true,
-		opts = {
-			enable_autocmd = false,
-		},
-	},
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    opts = {
+      enable_autocmd = false,
+    },
+  },
 
-	{
-		"numToStr/Comment.nvim",
+  {
+    "numToStr/Comment.nvim",
     event = { "BufReadPre", "BufNewFile" },
-	  config = function()
-			require("Comment").setup({
-				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-			})
+    config = function()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
     end
   },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+      require('lualine').setup({
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 1
+            }
+          }
+        }
+      })
+    end
+  }
 }
