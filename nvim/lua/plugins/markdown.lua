b99c27746f1c6d5ea178187fad710c87c05ee9cf
Andrei Gatej <andreigtj01@gmail.com>
2025-01-02 17:32:39 +0200

    feat: add `markdowny`
    For useful markdown mappings.

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,12 +1,21 @@
 return {
-  "jakewvincent/mkdnflow.nvim",
-  ft = { "markdown" },
-  config = function()
-    require("mkdnflow").setup({
-      mappings = {
-        MkdnEnter = {{'i', 'n', 'v'}, '<CR>'},
-        MkdnNewListItem = {'i', '<CR>'},
-      }
-    })
-  end,
+	{
+		"jakewvincent/mkdnflow.nvim",
+		ft = { "markdown" },
+		config = function()
+			require("mkdnflow").setup({
+				mappings = {
+					MkdnEnter = { { "i", "n", "v" }, "<CR>" },
+					MkdnNewListItem = { "i", "<CR>" },
+				},
+			})
+		end,
+	},
+	{
+		ft = { "markdown" },
+		"antonk52/markdowny.nvim",
+		config = function()
+			require("markdowny").setup()
+		end,
+	},
 }
