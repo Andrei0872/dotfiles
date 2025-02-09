e58c0ad2983ebbfff6e8cadc8c8eb87ed58dba6f
Andrei Gatej <andreigtj01@gmail.com>
2025-02-09 23:10:20 +0200

    feat(dap): remaps

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -7,73 +7,102 @@ local utils = require("dap.utils")
 dap.set_log_level("DEBUG")
 
 dap.adapters = {
-	["pwa-node"] = {
-		type = "server",
-		port = "${port}",
-		executable = {
-			command = "js-debug-adapter",
-			args = {
-				"${port}",
-			},
-		},
-	},
-	["codelldb"] = {
-		type = "server",
-		port = "${port}",
-		executable = {
-			command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
-			args = { "--port", "${port}" },
-		},
-	},
+  ["pwa-node"] = {
+    type = "server",
+    port = "${port}",
+    executable = {
+      command = "js-debug-adapter",
+      args = {
+        "${port}",
+      },
+    },
+  },
+  ["codelldb"] = {
+    type = "server",
+    port = "${port}",
+    executable = {
+      command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+      args = { "--port", "${port}" },
+    },
+  },
 }
 
 for _, language in ipairs({ "typescript", "javascript" }) do
-	dap.configurations[language] = {
-		{
-			type = "pwa-node",
-			request = "launch",
-			name = "Launch file",
-			program = "${file}",
-			cwd = "${workspaceFolder}",
-		},
-		{
-			type = "pwa-node",
-			request = "attach",
-			name = "Attach to process ID",
-			processId = utils.pick_process,
-			cwd = "${workspaceFolder}",
-		}
-	}
+  dap.configurations[language] = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
+    },
+    {
+      type = "pwa-node",
+      request = "attach",
+      name = "Attach to process ID",
+      processId = utils.pick_process,
+      cwd = "${workspaceFolder}",
+    },
+    {
+      type = "pwa-chrome",
+      request = "launch",
+      name = "Launch & Debug Chrome",
+      url = function()
+        local co = coroutine.running()
+        return coroutine.create(function()
+          vim.ui.input({
+            prompt = "Enter URL: ",
+            default = "http://localhost:3000",
+          }, function(url)
+            if url == nil or url == "" then
+              return
+            else
+              coroutine.resume(co, url)
+            end
+          end)
+        end)
+      end,
+      webRoot = vim.fn.getcwd(),
+      protocol = "inspector",
+      sourceMaps = true,
+      userDataDir = false,
+    },
+    {
+      name = "----- ↓ launch.json configs ↓ -----",
+      type = "",
+      request = "launch",
+    },
+  }
 end
 
 dap.configurations.rust = {
-	{
-		type = "codelldb",
-		request = "launch",
-		name = "Launch file",
-		cwd = "${workspaceFolder}",
-		stopOnEntry = false,
-		program = function()
-			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-		end,
-	},
+  {
+    type = "codelldb",
+    request = "launch",
+    name = "Launch file",
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+  },
 }
 
 dapui.setup({
-	icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
-	controls = {
-		icons = {
-			pause = "⏸",
-			play = "▶",
-			step_into = "⏎",
-			step_over = "⏭",
-			step_out = "⏮",
-			step_back = "b",
-			run_last = "▶▶",
-			terminate = "⏹",
-			disconnect = "⏏",
-		},
-	},
+  icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+  controls = {
+    icons = {
+      pause = "⏸",
+      play = "▶",
+      step_into = "⏎",
+      step_over = "⏭",
+      step_out = "⏮",
+      step_back = "b",
+      run_last = "▶▶",
+      terminate = "⏹",
+      disconnect = "⏏",
+    },
+  },
 })
 
 dap.listeners.after.event_initialized["dapui_config"] = dapui.open
@@ -81,27 +110,52 @@ dap.listeners.before.event_terminated["dapui_config"] = dapui.close
 dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
 local map = function(keys, func, desc)
-	if desc then
-		desc = "De[B]ugger: " .. desc
-	end
-	if keys then
-		keys = "<leader>b" .. keys
-	end
-	vim.keymap.set("n", keys, func, { desc = desc })
+  if desc then
+    desc = "[D]ebugger: " .. desc
+  end
+  if keys then
+    keys = "<leader>d" .. keys
+  end
+  vim.keymap.set("n", keys, func, { desc = desc })
 end
 
-map("s", dap.continue, "[S]tart/Continue")
+map("c", dap.continue, "[C]ontinue")
+-- TODO: is this really needed?
+-- map("a", function()
+--   require("dap").continue({ before = get_args })
+-- end, "Run with [A]rgs")
 map("i", dap.step_into, "Step [I]nto")
-map("v", dap.step_over, "Step O[V]er")
 map("o", dap.step_out, "Step [O]ut")
+map("O", dap.step_over, "Step Over")
+map("C", function()
+  require("dap").run_to_cursor()
+end, "Run to [C]ursor")
+map("g", function()
+  require("dap").goto_()
+end, "[G]o to line (no execute)")
 map("b", dap.toggle_breakpoint, "Toggle [B]reakpoint")
 map("B", function()
-	dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+  dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
 end, "Set [B]reakpoint")
+map("j", dap.down, "Down")
+map("k", dap.up, "Up")
+map("l", dap.run_last, "Run [L]ast")
+map("p", dap.pause, "Pause")
+map("r", function()
+  dap.repl.toggle()
+end, "Toggle REPL")
+map("s", dap.session, "Session")
+map("t", dap.terminate, "Terminate")
+map("w", function()
+  require("dap.ui.widgets").hover()
+end, "Widgets")
 
 map("u", function()
-	dapui.toggle({
-		-- Always open the nvim dap ui in the default sizes
-		reset = true,
-	})
+  dapui.toggle({
+    -- Always open the nvim dap ui in the default sizes
+    reset = true,
+  })
 end, "Toggle [U]I")
+map("e", function()
+  dapui.eval()
+end, "Eval")
