2bb0d82ce4c7a2d69b69b93c9b9ece438b0d7dec
Andrei Gatej <andreigtj01@gmail.com>
2025-02-17 20:10:28 +0200

    feat: toggle `:Vexplore`

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -87,4 +87,10 @@ map("n", "<C-u>", "<C-u>zz")
 
 map("x", "/", "<Esc>/\\%V")
 
-map("n", "<leader>e", "<cmd>:Ex<cr>")
+map("n", "<leader>e", function()
+  if vim.api.nvim_buf_get_option(0, "filetype") == "netrw" then
+    vim.api.nvim_exec("close", false)
+  else
+    vim.api.nvim_exec(":Vexplore", false)
+  end
+end, {})
