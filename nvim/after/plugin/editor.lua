1a6266389e0d856b7de21ac9f116cdeb1848b46e
Andrei Gatej <andreigtj01@gmail.com>
2025-02-18 11:16:35 +0200

    feat: set up `oil`

--- nvim/after/plugin/editor.lua
+++ nvim/after/plugin/editor.lua
@@ -4,6 +4,13 @@ require("todo-comments").setup({
   signs = false,
 })
 
-require("oil").setup()
+-- TODO: if preview open, use <C-{d,u}> for scrolling in preview mode.
+-- https://github.com/stevearc/oil.nvim/issues/41
+require("oil").setup({
+  -- ["<>"] = false,
+  -- ["\""] = { "actions.select", opts = { horizontal = true } },
+  --
+  -- ["<C-l>"] = false,
+  -- ["<leader>r"] = "actions.refresh",
+})
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
