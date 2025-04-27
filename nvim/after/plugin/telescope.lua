a89d83df77221262e748dd18b9ebb511f83fbbc6
Andrei Gatej <andreigtj01@gmail.com>
2025-04-27 20:55:06 +0300

    feat: preview in fuzzy finder

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -38,11 +38,7 @@ vim.keymap.set(
   { desc = "Recent buffers" }
 )
 vim.keymap.set("n", "<leader>ff", function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-    winblend = 10,
-    previewer = false,
-  }))
+  require("telescope.builtin").current_buffer_fuzzy_find()
 end, { desc = "[/] Fuzzily search in current buffer" })
 vim.keymap.set(
   "n",
