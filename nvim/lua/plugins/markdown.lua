0f494432da1502b8bb04e264767fea0f8dc6938c
Andrei Gatej <andreigtj01@gmail.com>
2026-08-14 12:32:16 +0300

    feat: add markdown preview plugin

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -7,4 +7,13 @@ return {
     ft = { "markdown" },
     "antonk52/markdowny.nvim",
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
